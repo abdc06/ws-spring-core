5943757ef7e28187df51782db7454c9077f1036b
abdc06 <abdc2806@gmail.com>
2022-10-28 23:29:17 +0900

    spring-message 프로젝트 생성

---
+++ spring-message/src/test/java/me/abdc/springmessage/SpringMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package me.abdc.springmessage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMessageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
