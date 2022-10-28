35206f89d5d9e9f99d37fdcc8a371315af9175ea
abdc06 <abdc2806@gmail.com>
2022-10-28 23:42:48 +0900

    spring-event-publisher 프로젝트 생성

---
+++ spring-event-publisher/src/main/java/me/abdc/springeventpublisher/SpringEventPublisherApplication.java
@@ -0,0 +1,13 @@
+package me.abdc.springeventpublisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEventPublisherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEventPublisherApplication.class, args);
+    }
+
+}
---
+++ spring-event-publisher/src/test/java/me/abdc/springeventpublisher/SpringEventPublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package me.abdc.springeventpublisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEventPublisherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
