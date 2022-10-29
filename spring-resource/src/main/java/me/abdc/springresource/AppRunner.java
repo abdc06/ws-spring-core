89fafad3d5378459b1223596f350d41ac0a2eaf3
abdc06 <abdc2806@gmail.com>
2022-10-29 10:24:00 +0900

    Resource 추상화에 따른 기준 경로차이 확인

--- spring-resource/src/main/java/me/abdc/springresource/AppRunner.java
+++ spring-resource/src/main/java/me/abdc/springresource/AppRunner.java
@@ -3,6 +3,7 @@ package me.abdc.springresource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.ApplicationContext;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
@@ -16,6 +17,9 @@ public class AppRunner implements ApplicationRunner {
     @Autowired
     ResourceLoader loader;
 
+    @Autowired
+    ApplicationContext context;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
         Resource resource = loader.getResource("classpath:text.txt");
@@ -25,5 +29,21 @@ public class AppRunner implements ApplicationRunner {
         if (resource.exists()) {
             System.out.println(Files.readString(Path.of(resource.getURI())));
         }
+
+        /********************************************************************/
+        Resource resource1 = context.getResource("classpath:text.txt");
+        Resource resource2 = context.getResource("text.txt");
+
+        System.out.println(resource1.getClass());   //ClassPathResource
+        System.out.println(resource2.getClass());   //ServletContextResource
+
+        System.out.println(resource1.exists()); //true
+        System.out.println(resource2.exists()); //false
+
+        System.out.println(resource1.getDescription());
+        System.out.println(resource2.getDescription());
+
+        System.out.println(Files.readString(Path.of(resource1.getURI())));
+        System.out.println(Files.readString(Path.of(resource2.getURI())));  //FileNotFoundException
     }
 }
