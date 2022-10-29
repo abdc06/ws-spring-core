5f778b6b199c6d4aa4efe6c566dce3211b2fa550
abdc06 <abdc2806@gmail.com>
2022-10-29 10:58:38 +0900

    Validator 직접 구현한 방법

---
+++ spring-validation/src/main/java/me/abdc/springvalidation/AppRunner.java
@@ -0,0 +1,31 @@
+package me.abdc.springvalidation;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.Errors;
+
+import java.util.Arrays;
+
+@Component
+public class AppRunner implements ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        Event event = new Event();
+        EventValidator eventValidator = new EventValidator();
+        Errors errors = new BeanPropertyBindingResult(event, "event");
+
+        eventValidator.validate(event, errors);
+
+        System.out.println(errors.hasErrors());
+
+        errors.getAllErrors().forEach(e -> {
+            Arrays.stream(e.getCodes()).forEach(System.out::println);
+            System.out.println(e.getDefaultMessage());
+        });
+
+
+    }
+}
---
+++ spring-validation/src/main/java/me/abdc/springvalidation/EventValidator.java
@@ -0,0 +1,18 @@
+package me.abdc.springvalidation;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+public class EventValidator implements Validator {
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return Event.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "title", "not empty", "Empty title is not allowed");
+    }
+}
