2ea938515c6a6fb42d87d008c9eaccc2eca3cd35
abdc06 <abdc2806@gmail.com>
2022-10-29 09:58:21 +0900

    spring-resource 프로젝트 생성

---
+++ spring-resource/src/test/java/me/abdc/springresource/SpringResourceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.abdc.springresource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringResourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
