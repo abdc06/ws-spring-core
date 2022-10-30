d6adf0670e8c8446299ff968d8fc5f689e97befa
abdc06 <abdc2806@gmail.com>
2022-10-30 15:03:44 +0900

    spring-data-binding 프로젝트 생성

---
+++ spring-data-binding/src/main/java/me/abdc/springdatabinding/SpringDataBindingApplication.java
@@ -0,0 +1,13 @@
+package me.abdc.springdatabinding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataBindingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataBindingApplication.class, args);
+    }
+
+}
