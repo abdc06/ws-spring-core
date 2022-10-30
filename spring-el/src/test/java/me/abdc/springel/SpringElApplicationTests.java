f8ce6af0a7a748d881dd13b298ee49b0186403e0
abdc06 <abdc2806@gmail.com>
2022-10-30 15:48:17 +0900

    spring-el 프로젝트 생성

---
+++ spring-el/src/test/java/me/abdc/springel/SpringElApplicationTests.java
@@ -0,0 +1,13 @@
+package me.abdc.springel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringElApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
